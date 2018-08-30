8bc6b1c7180de6e334beb63219cd9f21450c3e7c
rodrigo alvarez <inglaterra_dhoko@hotmail.com>
2018-08-30 11:00:44 -0500

    Intercambio inicial de proyecto

---
+++ Alumno.java
@@ -0,0 +1,34 @@
+
+public class Alumno
+{ private String nombre;
+  private int clave;
+    
+  
+  
+  public  Alumno(String nombre,int clave)
+  {
+      
+     this.nombre=
+      
+      
+      
+  }
+    
+    
+   public void
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Laboratorio.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList; 
+
+public class Laboratorio
+{
+    private ArrayList<String> lista;
+    
+    
+    
+    public Laboratorio()
+    {
+     
+        lista= new ArrayList<String>();
+        
+        
+    }
+    
+    
+    
+    
+    public void darDeBaja()
+    {
+        
+        
+    }
+    
+    
+    
+    
+    public void daDeAlta()
+    {
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
